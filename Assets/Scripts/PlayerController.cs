274f925c6a9afe313eb6911225bbc555e9fbefaa
James Shipp <jmscshipp202@gmail.com>
2021-12-27 15:50:57 -0500

    got a lil mouse raycast position thang going on !

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 pointerPos;
+    RaycastHit hit;
+    int layerMask;
+    public GameObject pointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (LayerMask.NameToLayer("ground") > 0)
+        {
+            layerMask = 1 << LayerMask.NameToLayer("ground");
+        }
+        else
+        {
+            layerMask = 0;
+            Debug.LogError("ground layer not set correctly!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, layerMask))
+        {
+            pointerPos = hit.point;
+        }
+
+        pointer.transform.position = pointerPos;
+    }
+}
