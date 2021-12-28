5d5b4a9c413ceb3673aabd627b0fd90aa2b210ac
Arthur Hertz <arthertz@iu.edu>
2021-12-27 19:37:59 -0500

    traffic jam 2 player demo bonanza

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,35 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     public GameObject pointer;
+    GameObject pointerInstance;
     public GameObject truck;
+    GameObject truckInstance;
     public Material[] pointerMats = new Material[2];
     private static int playerIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = GameObject.FindGameObjectWithTag("playerInputManager").transform.position;
-        pointer = Instantiate(pointer, transform.position, Quaternion.identity);
-        GetComponentInChildren<PlayerInput>().SetPointer(pointer);
-        truck = Instantiate(truck, new Vector3(transform.position.x + Random.Range(-5f, 5f), transform.position.y, transform.position.z + Random.Range(-5f, 5f)) , Quaternion.identity);
+        Vector3 inputManPos = GameObject.FindGameObjectWithTag("playerInputManager").transform.position;
+        transform.position = inputManPos;
+        truckInstance = Instantiate(truck, new Vector3(transform.position.x + Random.Range(-5f, 5f), transform.position.y, transform.position.z + Random.Range(-5f, 5f)) , Quaternion.identity);
+        pointerInstance = GameObject.Instantiate(pointer, truckInstance.transform.position, Quaternion.identity);
+        GetComponentInChildren<PlayerInput>().SetPointer(pointerInstance);
 
         playerIndex++;
         if (playerIndex > 1)
-            pointer.GetComponent<MeshRenderer>().material = pointerMats[0];
+            pointerInstance.GetComponent<MeshRenderer>().material = pointerMats[0];
         else
-            pointer.GetComponent<MeshRenderer>().material = pointerMats[1];
+            pointerInstance.GetComponent<MeshRenderer>().material = pointerMats[1];
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        truck.GetComponent<NavMeshAgent>().SetDestination(pointer.transform.position);
+        if (!pointerInstance) return;
+        if (!truckInstance) return;
+
+        truckInstance.GetComponent<NavMeshAgent>().SetDestination(pointerInstance.transform.position);
     }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -13,21 +13,22 @@ public class PlayerInput : MonoBehaviour
 {
     public float speed = 5f;
     private GameObject pointer;
+    private bool initialized = false;
 
     mouseInput myInput;
-    Vector3 location = Vector3.zero;
+    Vector3 location;
 
     Vector2 movementInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (!initialized) return;
+        Debug.Log("Initialized");
+        if (!pointer) return;
+        Debug.Log("Pointer exists");
+
         location.x = myInput.x;
         location.z = myInput.y;
 
@@ -42,7 +43,11 @@ public class PlayerInput : MonoBehaviour
 
     public void SetPointer(GameObject pointer)
     {
+        this.location = transform.position;
+        myInput.x = location.x;
+        myInput.y = location.z;
         this.pointer = pointer;
+        initialized = true;
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
